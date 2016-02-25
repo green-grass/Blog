5c04b26eac2f30d08ad8a8a1a4b0b389212bb566
mnguyen284 <mnguyen284@gmail.com>
2016-02-25 17:07:58 +0700

    Update

---
+++ samples/PhantomNet.Blog.Sample/Controllers/RemoteImageFolderController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.PlatformAbstractions;
+using PhantomNet.RemoteFolder;
+
+namespace PhantomNet.Blog.Sample.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/remote-image-folder")]
+    public class RemoteImageFolderController : Controller
+    {
+        private readonly RemoteImageFolderService<RemoteImage> _remoteFolder;
+
+        private readonly string _virtualPath = "~/content/blog/images";
+        private readonly string _thumbsVirtualPath = "~/content/blog/thumbs";
+
+        public RemoteImageFolderController(RemoteImageFolderService<RemoteImage> remoteFolder)
+        {
+            _remoteFolder = remoteFolder;
+        }
+
+        [HttpGet]
+        public IEnumerable<RemoteImage> GetImages()
+        {
+            return _remoteFolder.List(_virtualPath, _thumbsVirtualPath, true);
+        }
+
+        [HttpGet("{id}", Name = "GetImage")]
+        public async Task<IActionResult> GetImage([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            var image = await Task.FromResult(new RemoteImage());
+
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+
+            return Ok(image);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(ICollection<IFormFile> files)
+        {
+            var result = await _remoteFolder.UploadAsync(files, _virtualPath);
+            return CreatedAtRoute("GetImage", new { id = "abc" }, result);
+        }
+    }
+}
---
+++ src/PhantomNet.Blog/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PhantomNet.Blog")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhantomNet.Blog")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eaeaf54c-5884-45e4-9ef1-5fc8d7574592")]
